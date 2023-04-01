31bd408d7e46855875847f8f4c980a68ca68b81c
bohdan <bohdanstep23@gmail.com>
2023-04-01 22:09:14 +0300

    done

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,16 @@
+import random
 
+n = int(input("Введіть кількість елементів списку: "))
+nums = [random.randint(1, 100) for _ in range(n)]
+
+even_count = 0
+odd_count = 0
+
+for num in nums:
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print("Кількість парних чисел:", even_count)
+print("Кількість непарних чисел:", odd_count)
